280dc293c3db993d3918100a38b67d5ba94498d7
ASHUVINAYAK1 <Ashuvinayak1508@gmail.com>
2024-02-29 19:03:38 +0530

    FIRST COMMIT

---
+++ src/test/java/techmovies/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package techmovies.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
